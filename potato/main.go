7015e8720e2f724ad6f3deae0b0b3744ace6e573
guilherme <guile.hm@hotmail.com>
2021-11-22 14:07:59 -0300

    create message handler

--- potato/main.go
+++ potato/main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"github.com/bwmarrin/discordgo"
 )
 
-func main() {
-	fmt.Println("hello from go potato")
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	if m.Content == "ping" {
+		_, err := s.ChannelMessageSend(m.ChannelID, "pong!")
+		if err != nil {
+			fmt.Println("could not send message for channel: " + m.ChannelID)
+		}
+	}
+
 }
