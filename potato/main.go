ea5f9e4a381b2d1b9b957a895b88f01503d98d5f
guilherme <guile.hm@hotmail.com>
2021-11-22 14:09:59 -0300

    create bot and add message handler

--- potato/main.go
+++ potato/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/bwmarrin/discordgo"
+	"os"
 )
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
@@ -16,5 +17,18 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 			fmt.Println("could not send message for channel: " + m.ChannelID)
 		}
 	}
+
+}
+
+func main() {
+
+	discordToken := os.Getenv("DISCORD_TOKEN")
+	discord, err := discordgo.New("Bot " + discordToken)
+	if err != nil {
+		fmt.Println("error starting bot", err)
+		return
+	}
+
+	discord.AddHandler(messageCreate)
 
 }
