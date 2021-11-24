48d9c8567f020db48ac4c619a5f4624f725b66c0
guilherme <guile.hm@hotmail.com>
2021-11-24 15:08:30 -0300

    move handlers

---
+++ potato/handlers/discord-handlers.go
@@ -0,0 +1,20 @@
+package handlers
+
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+)
+
+func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	if m.Content == "ping" {
+		_, err := s.ChannelMessageSend(m.ChannelID, "pong!")
+		if err != nil {
+			fmt.Println("could not send message for channel: " + m.ChannelID)
+		}
+	}
+
+}
--- potato/main.go
+++ potato/main.go
@@ -2,26 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/guilehm/go-potato/handlers"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 )
-
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-
-	if m.Author.ID == s.State.User.ID {
-		return
-	}
-	if m.Content == "ping" {
-		_, err := s.ChannelMessageSend(m.ChannelID, "pong!")
-		if err != nil {
-			fmt.Println("could not send message for channel: " + m.ChannelID)
-		}
-	}
-
-}
 
 func main() {
 	tmdbAccessToken := os.Getenv("TMDB_ACCESS_TOKEN")
@@ -43,7 +30,7 @@ func main() {
 		fmt.Println("error opening connection,", err)
 		return
 	}
-	discord.AddHandler(messageCreate)
+	discord.AddHandler(handlers.MessageCreate)
 
 	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
