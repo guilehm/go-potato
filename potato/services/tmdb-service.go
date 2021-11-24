257076082dcc2f72fe598658279aa9d48d7ada4f
guilherme <guile.hm@hotmail.com>
2021-11-24 12:49:08 -0300

    create function to deal with requests

--- potato/services/tmdb-service.go
+++ potato/services/tmdb-service.go
@@ -2,6 +2,9 @@ package services
 
 import (
 	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 const BaseApiUrl = "https://api.themoviedb.org/3/"
@@ -15,3 +18,26 @@ type tmdbService struct {
 func (t *tmdbService) Search(text string) error {
 	return nil
 }
+
+func (t *tmdbService) makeRequest(endpoint string) ([]byte, error) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%v%v", BaseApiUrl, endpoint), nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", t.AccessToken))
+	req.Header.Set("Content-Type", "application/json;charset=utf-8")
+
+	resp, err := http.DefaultClient.Do(req)
+
+	if resp.StatusCode == http.StatusNotFound {
+		return nil, ErrNotFound
+	}
+	defer resp.Body.Close()
+
+	body, err :=  ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
+
