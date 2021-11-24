69d8a892c38c29e02063e8da19fea6f5535015e1
guilherme <guile.hm@hotmail.com>
2021-11-24 18:34:08 -0300

    update handler to message results

--- potato/handlers/discord-handlers.go
+++ potato/handlers/discord-handlers.go
@@ -2,10 +2,15 @@ package handlers
 
 import (
 	"fmt"
+	"os"
 	"strings"
+
+	"github.com/guilehm/go-potato/services"
 
 	"github.com/bwmarrin/discordgo"
 )
+
+var service = services.TMDBService{AccessToken: os.Getenv("TMDB_ACCESS_TOKEN")}
 
 func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	_ = s.ChannelTyping(m.ChannelID)
@@ -23,10 +28,13 @@ func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	if strings.HasPrefix(m.Content, ".s") {
 		text := strings.Trim(m.Content[3:], " ")
-		_, err := s.ChannelMessageSend(m.ChannelID, "searching for: "+text)
+		s.ChannelMessageSend(m.ChannelID, "searching for: "+text)
+		searchResponse, err := service.SearchMovie(text)
 		if err != nil {
-			fmt.Println("could not send message for channel: " + m.ChannelID)
-			fmt.Println(err.Error())
+			s.ChannelMessageSend(m.ChannelID, "Could not search. Please try again later."+err.Error())
+		}
+		for _, result := range searchResponse.Results {
+			s.ChannelMessage(m.ChannelID, result.Title)
 		}
 	}
 
