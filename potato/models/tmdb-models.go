a86c56016979a14e768508f61ec9528116dbcbe2
guilherme <guile.hm@hotmail.com>
2021-11-24 13:33:28 -0300

    create movie search result model

---
+++ potato/models/tmdb-models.go
@@ -0,0 +1,18 @@
+package models
+
+type MovieSearchResult struct {
+	Adult            bool    `json:"adult"`
+	BackdropPath     string  `json:"backdrop_path"`
+	GenreIds         []int   `json:"genre_ids"`
+	ID               int     `json:"id"`
+	OriginalLanguage string  `json:"original_language"`
+	OriginalTitle    string  `json:"original_title"`
+	Overview         string  `json:"overview"`
+	Popularity       float64 `json:"popularity"`
+	PosterPath       string  `json:"poster_path"`
+	ReleaseDate      string  `json:"release_date"`
+	Title            string  `json:"title"`
+	Video            bool    `json:"video"`
+	VoteAverage      int     `json:"vote_average"`
+	VoteCount        int     `json:"vote_count"`
+}
