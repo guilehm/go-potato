56b245d1cadf18df689bbbed8065bb50b87ff29c
guilherme <guile.hm@hotmail.com>
2021-11-25 22:21:45 -0300

    create struct for tv search result

--- potato/models/tmdb-models.go
+++ potato/models/tmdb-models.go
@@ -23,3 +23,19 @@ type MovieSearchResponse struct {
 	TotalPages   int                 `json:"total_pages"`
 	TotalResults int                 `json:"total_results"`
 }
+
+type TVSearchResult struct {
+	BackdropPath     string   `json:"backdrop_path"`
+	FirstAirDate     string   `json:"first_air_date"`
+	GenreIds         []int    `json:"genre_ids"`
+	Id               int      `json:"id"`
+	Name             string   `json:"name"`
+	OriginCountry    []string `json:"origin_country"`
+	OriginalLanguage string   `json:"original_language"`
+	OriginalName     string   `json:"original_name"`
+	Overview         string   `json:"overview"`
+	Popularity       float64  `json:"popularity"`
+	PosterPath       string   `json:"poster_path"`
+	VoteAverage      float64  `json:"vote_average"`
+	VoteCount        int      `json:"vote_count"`
+}
