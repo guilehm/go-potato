4905a0a3efd574e1cb8ada56d9c76253c391fa8e
guilherme <guile.hm@hotmail.com>
2021-11-26 19:42:50 -0300

    create mongo connection

---
+++ potato/db/mongo.go
@@ -0,0 +1,22 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func Connection() *mongo.Client {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("connected to MongoDB")
+	return client
+}
