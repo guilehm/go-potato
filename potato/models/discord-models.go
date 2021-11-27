f463ccaddbac666599609066380eca8ba6d596a1
guilherme <guile.hm@hotmail.com>
2021-11-27 14:54:07 -0300

    create user discord struct

--- potato/models/discord-models.go
+++ potato/models/discord-models.go
@@ -1,8 +1,15 @@
 package models
 
+import "github.com/bwmarrin/discordgo"
+
 type MessageData struct {
 	MessageID  string `json:"message_id" bson:"message_id"`
 	Text       string `json:"text" bson:"text"`
 	Page       int    `json:"page" bson:"page"`
 	TotalPages int    `json:"total_pages" bson:"total_pages"`
+}
+
+type UserDiscord struct {
+	discordgo.User `bson:",inline"`
+	AvatarUrl      string `json:"avatar_url" bson:"avatar_url"`
 }
