429daa3f501d131c4c9609bf2830a3069082ef96
guilherme <guile.hm@hotmail.com>
2021-11-30 22:53:06 -0300

    move hello and ping handlers from event-handlers file

--- potato/handlers/event-handlers.go
+++ potato/handlers/event-handlers.go
@@ -1,18 +1,8 @@
 package handlers
 
 import (
-	"context"
-	"fmt"
 	"os"
 	"strings"
-	"time"
-
-	"github.com/guilehm/go-potato/models"
-
-	"github.com/guilehm/go-potato/db"
-
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"github.com/guilehm/go-potato/services"
 
@@ -80,49 +70,6 @@ func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if strings.HasPrefix(m.Content, ".tl") {
 		handleMyTVShowList(s, m)
 		return
-	}
-
-}
-
-func handlePing(s *discordgo.Session, m *discordgo.MessageCreate) {
-	_ = s.ChannelTyping(m.ChannelID)
-	_, err := s.ChannelMessageSend(m.ChannelID, "pong!")
-	if err != nil {
-		fmt.Println("could not send message for channel: " + m.ChannelID)
-		fmt.Println(err.Error())
-	}
-}
-
-func handleHello(s *discordgo.Session, m *discordgo.MessageCreate) {
-	_ = s.ChannelTyping(m.ChannelID)
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	upsert := true
-	opt := options.UpdateOptions{Upsert: &upsert}
-	now, _ := time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
-	user := models.UserDiscord{
-		User:        *m.Author,
-		AvatarUrl:   m.Author.AvatarURL(""),
-		DateChanged: now,
-		Likes:       []int{},
-	}
-
-	result, err := db.UsersCollection.UpdateOne(
-		ctx, bson.M{"id": m.Author.ID}, bson.D{{Key: "$set", Value: &user}}, &opt,
-	)
-
-	var t string
-	if result.UpsertedID != nil {
-		t = "create"
-	} else {
-		t = "update"
-	}
-	if err != nil {
-		_, _ = s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Could not %v user", t))
-	} else {
-		_, _ = s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("User successfully %vd!", t))
 	}
 
 }
--- potato/handlers/user-handlers.go
+++ potato/handlers/user-handlers.go
@@ -13,6 +13,40 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+func handleHello(s *discordgo.Session, m *discordgo.MessageCreate) {
+	_ = s.ChannelTyping(m.ChannelID)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	upsert := true
+	opt := options.UpdateOptions{Upsert: &upsert}
+	now, _ := time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	user := models.UserDiscord{
+		User:        *m.Author,
+		AvatarUrl:   m.Author.AvatarURL(""),
+		DateChanged: now,
+		Likes:       []int{},
+	}
+
+	result, err := db.UsersCollection.UpdateOne(
+		ctx, bson.M{"id": m.Author.ID}, bson.D{{Key: "$set", Value: &user}}, &opt,
+	)
+
+	var t string
+	if result.UpsertedID != nil {
+		t = "create"
+	} else {
+		t = "update"
+	}
+	if err != nil {
+		_, _ = s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Could not %v user", t))
+	} else {
+		_, _ = s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("User successfully %vd!", t))
+	}
+
+}
+
 func handleMyTVShowList(s *discordgo.Session, m *discordgo.MessageCreate) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
