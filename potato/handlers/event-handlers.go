26a235f2fb3a4b790b0abd0941f8e7e50034ab93
guilherme <guile.hm@hotmail.com>
2021-11-30 22:48:44 -0300

    create ping handler

--- potato/handlers/event-handlers.go
+++ potato/handlers/event-handlers.go
@@ -89,6 +89,15 @@ func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 }
 
+func handlePing(s *discordgo.Session, m *discordgo.MessageCreate) {
+	_ = s.ChannelTyping(m.ChannelID)
+	_, err := s.ChannelMessageSend(m.ChannelID, "pong!")
+	if err != nil {
+		fmt.Println("could not send message for channel: " + m.ChannelID)
+		fmt.Println(err.Error())
+	}
+}
+
 func handleHello(s *discordgo.Session, m *discordgo.MessageCreate) {
 	_ = s.ChannelTyping(m.ChannelID)
 
