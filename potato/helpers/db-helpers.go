62f9e1d10f37e5fc69c4e21c87d44e2cbddf34a9
guilherme <guile.hm@hotmail.com>
2021-12-02 19:54:25 -0300

    create helper to update tv show detail

---
+++ potato/helpers/db-helpers.go
@@ -0,0 +1,45 @@
+package helpers
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/guilehm/go-potato/db"
+	"github.com/guilehm/go-potato/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func UpdateTVShowDetail(tvShow models.TVShowResult, message *discordgo.Message) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	upsert := true
+	opt := options.UpdateOptions{Upsert: &upsert}
+
+	_, err := db.TVShowsCollection.UpdateOne(
+		ctx, bson.M{"id": tvShow.ID}, bson.D{{Key: "$set", Value: tvShow}}, &opt,
+	)
+	if err != nil {
+		fmt.Println("could not update TV Show #", tvShow.ID)
+	}
+
+	if err != nil {
+		fmt.Println("Could not convert TV Show ID #", tvShow.ID)
+		return
+	}
+
+	messageData := models.MessageData{
+		MessageID:    message.ID,
+		Type:         models.TD,
+		ContentId:    tvShow.ID,
+		ContentTitle: tvShow.Name,
+	}
+	_, err = db.MessagesDataCollection.InsertOne(ctx, messageData)
+	if err != nil {
+		fmt.Println("could save message data for tv-show #", tvShow.ID)
+	}
+}
