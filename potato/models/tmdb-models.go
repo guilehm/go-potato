9bffaa037e2c4c3d0fceac72d31f295c91daae54
guilherme <guile.hm@hotmail.com>
2021-12-04 12:14:27 -0300

    create movie result struct

--- potato/models/tmdb-models.go
+++ potato/models/tmdb-models.go
@@ -148,3 +148,31 @@ type SpokenLanguage struct {
 	Iso6391     string `json:"iso_639_1"`
 	Name        string `json:"name"`
 }
+
+type MovieResult struct {
+	Adult               bool                `json:"adult"`
+	BackdropPath        interface{}         `json:"backdrop_path"`
+	BelongsToCollection interface{}         `json:"belongs_to_collection"`
+	Budget              int                 `json:"budget"`
+	Genres              []Genres            `json:"genres"`
+	Homepage            string              `json:"homepage"`
+	ID                  int                 `json:"id"`
+	ImdbID              string              `json:"imdb_id"`
+	OriginalLanguage    string              `json:"original_language"`
+	OriginalTitle       string              `json:"original_title"`
+	Overview            string              `json:"overview"`
+	Popularity          float64             `json:"popularity"`
+	PosterPath          interface{}         `json:"poster_path"`
+	ProductionCompanies []ProductionCompany `json:"production_companies"`
+	ProductionCountries []ProductionCountry `json:"production_countries"`
+	ReleaseDate         string              `json:"release_date"`
+	Revenue             int                 `json:"revenue"`
+	Runtime             int                 `json:"runtime"`
+	SpokenLanguages     []SpokenLanguage    `json:"spoken_languages"`
+	Status              string              `json:"status"`
+	Tagline             string              `json:"tagline"`
+	Title               string              `json:"title"`
+	Video               bool                `json:"video"`
+	VoteAverage         float64             `json:"vote_average"`
+	VoteCount           int                 `json:"vote_count"`
+}
