9842075575e32c483e0ca2bae635d66100c60118
guilherme <guile.hm@hotmail.com>
2021-12-15 19:41:20 -0300

    send stock message

--- potato/handlers/stocks-handlers.go
+++ potato/handlers/stocks-handlers.go
@@ -7,6 +7,15 @@ import (
 )
 
 func handleStockSearch(s *discordgo.Session, m *discordgo.MessageCreate) {
+	_ = s.ChannelTyping(m.ChannelID)
 	symbol := strings.Trim(m.Content[4:], " ")
 	_, _ = s.ChannelMessageSend(m.ChannelID, "searching for "+symbol)
+
+	stock, err := stocksService.SearchStockPrice(symbol)
+	if err != nil {
+		return
+	}
+	_ = s.ChannelTyping(m.ChannelID)
+	_, _ = s.ChannelMessageSend(m.ChannelID, stock.CompanyName)
+
 }
