a127ccd470d18d96655768643c82ffe49268fe61
guilherme <guile.hm@hotmail.com>
2021-12-15 19:37:12 -0300

    create make request helper function

--- potato/services/stocks-service.go
+++ potato/services/stocks-service.go
@@ -3,6 +3,10 @@ package services
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
 
 	"github.com/guilehm/go-potato/potato/models"
 )
@@ -11,6 +15,44 @@ const BaseStockAPIURL = "https://api.hgbrasil.com/finance/"
 
 type StocksService struct {
 	SecretKey string
+}
+
+func (s StocksService) makeRequest(endpoint string, queries url.Values) ([]byte, error) {
+	u, err := url.Parse(fmt.Sprintf("%v%v", BaseStockAPIURL, endpoint))
+	if err != nil {
+		return nil, err
+	}
+
+	q := u.Query()
+	q.Set("key", os.Getenv("STOCKS_API_SECRET_KEY"))
+	for key, values := range queries {
+		for _, v := range values {
+			q.Set(key, v)
+		}
+	}
+	u.RawQuery = q.Encode()
+
+	req, err := http.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode == http.StatusNotFound {
+		return nil, ErrNotFound
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+
 }
 
 func (s StocksService) unmarshallStockPriceResponse(body []byte) (*models.StockPriceResponse, error) {
