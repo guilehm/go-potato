7f3cd82eb4a5a12ce9489f1d648928fa3fae9baa
guilherme <guile.hm@hotmail.com>
2021-12-15 19:46:30 -0300

    update message

--- potato/handlers/stocks-handlers.go
+++ potato/handlers/stocks-handlers.go
@@ -9,7 +9,7 @@ import (
 func handleStockSearch(s *discordgo.Session, m *discordgo.MessageCreate) {
 	_ = s.ChannelTyping(m.ChannelID)
 	symbol := strings.Trim(m.Content[4:], " ")
-	_, _ = s.ChannelMessageSend(m.ChannelID, "searching for "+symbol)
+	_, _ = s.ChannelMessageSend(m.ChannelID, "searching for "+symbol+"...")
 
 	stock, err := stocksService.SearchStockPrice(symbol)
 	if err != nil {
